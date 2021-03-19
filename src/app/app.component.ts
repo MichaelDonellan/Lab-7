9b0d42d75e4fb5f11e1b5232ad0e98943069f1e6
Michael <you@example.commichaeldonellan1999@gmail.com>
2021-03-19 13:07:42 +0000

    Lab 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {DataService} from './Services/data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'data-app';
+students:any = [];
+weather:any = [];
+  constructor(private dataSevice:DataService){}
+
+ngOnInit(){
+this.dataSevice.GetStudentData().subscribe(
+  (data)=>{
+
+    this.students = data.students;
+    console.log(this.students);
+  }
+
+);
+this.dataSevice.GetStudentData().subscribe(
+  (data)=>{
+
+    this.weather = data.weather;
+    console.log(this.weather);
+  }
+
+);
+
+}
+
 }
